99997571e3bfdc741851b41497653d3034c20850
Seven.Chi <chiyaoching@gmail.com>
2020-05-24 15:50:25 +0800

    modify: changed the main layout.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template lang="pug">
   v-app
-    Drawer(:drawerFlag.sync="drawerObj.model")
-    Header(:drawerFlag.sync="drawerObj.model")
-
+    Header
     v-content
       router-view 
     Footer
@@ -10,29 +8,22 @@
 
 <script>
 import Header from '@/views/layout/Header'
-import Drawer from '@/views/layout/Drawer'
 import Footer from '@/views/layout/Footer'
 
 export default {
   name: 'App',
   data: () => ({
-    drawerObj: {
-      model: null
-    }
   }),
   components: {
     Header,
-    Drawer,
     Footer
   },
   mounted () {
-    console.log(window.firestore)
-    this.$store.commit('SET_CURRENT_ROLE', this.roles.find(role => role.link === this.$route.path))
   },
   computed: {
-    roles () {
-      return this.$store.getters.getRoleList
-    },
   }
 };
 </script>
+
+<style lang="sass">
+</style>
--- src/views/layout/Header.vue
+++ src/views/layout/Header.vue
@@ -1,38 +1,26 @@
 <template lang="pug">
   v-app-bar(app)
-    v-app-bar-nav-icon(@click.stop="drawerController")
-    v-toolbar-title {{role && role.text}}
-    //- v-row(style="position: absolute; width: 100%; z-index: -1" v-if="role && role.type === 'player'")
-    //-   v-col(cols="12")
-    //-     v-row(justify="center")
-    //-       v-toolbar-title {{headerText}}
+    v-avatar
+      v-img(:src="require('@/assets/img/smartbee.png')")
+    v-tabs
+      v-tab(v-for="item in pages" :key="item.link" link :to="item.link") {{item.text}}
+    v-spacer
+    v-avatar(color="primary")
+      span(class="black--text") User
 </template>
 
 <script>
 export default {
   props: {
-    drawerFlag: {
-      type: Boolean,
-      default: false
-    }
   },
+  data: () => ({
+  }),
   computed: {
-    role: {
-      get () {
-        return this.$store.getters.getCurrentRole
-      },
-      set (value) {
-        this.$store.commit('SET_CURRENT_ROLE', value)
-      }
-    },
-    headerText () {
-      return this.$store.getters.getHeaderText
+    pages () {
+      return this.$store.getters.getPageList
     }
   },
   methods: {
-    drawerController () {
-      this.$emit('update:drawerFlag', !this.drawerFlag)
-    }
   }
 }
 </script>
