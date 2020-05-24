cb197afc5f4f38519a1af715f7b086a2fb5782b4
Seven.Chi <chiyaoching@gmail.com>
2020-05-24 15:52:37 +0800

    add: add the chat page.

---
+++ src/views/chat/Chat.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  v-container(fluid)
+    v-row
+      v-col(cols="auto")
+        v-btn(outlined @click="$router.push('/chat')" color="primary") Back To list
+</template>
+
+<script>
+export default {
+  mounted () {
+    console.log(this.$router)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
